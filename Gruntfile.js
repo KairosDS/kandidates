e6284cac35c46e3080dcb192123dbb049cd8f4d7
Jago (Carlos Santiago Melón) <santiago.melon@kairosds.com>
2015-09-08 12:21:20 +0200

    Enhanced Grunt + created task to run+open

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,57 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
+  //Some helper vars
+  var host = '127.0.0.1',
+      port = 9000,
+      deafultOpen = '/app',
+      gruntTarget = grunt.option('target') || 'dev';
+
+  // Project and tasks configuration.
   grunt.initConfig({
+    // Read main info from package.json
     pkg: grunt.file.readJSON('package.json'),
-    serve: {
-        options: {
-            port: 9000,
-            aliases: {
-              'index.html': {
-                tasks: [],
-                output: 'app/index.html'
-              }
-            }
-        }
+
+    // Initial setup for grunt-http-server
+    'http-server': {
+      'dev': {
+        root: '.',
+        port: port,
+        host: host,
+        //cache: <sec>,
+        showDir : true,
+        autoIndex: true,
+        ext: "html", //server default file extension 
+
+        // run in parallel with other tasks 
+        runInBackground: false, //if set to true it auto-stops :(
+
+        //logger function
+        logFn: function(req, res, error) { },
+
+        //https: { //can be set to just https: true
+        //    cert: "cert.pem",
+        //    key : "key.pem"
+        //}
+      }
+    },
+
+    // Initial setup for grunt-open
+    open: {
+      dev: {
+        path: 'http://' + host + ':' + port + deafultOpen
+      }
     }
   });
 
   // Load tasks
-  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-http-server');
 
-  // Register tasks to be run from package.json by npm
-  grunt.registerTask('start', ['grunt-serve']);
+  // Register tasks to be run from cli (grunt task)
+  grunt.registerTask('run', ['http-server']);
+  grunt.registerTask('runopen', ['open:' + gruntTarget, 'http-server']);
 
   // Register default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['open:' + gruntTarget, 'http-server']);
   
 };

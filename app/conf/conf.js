3d66253db243082b7c6dfb197539b06a0375132f
Jago (Carlos Santiago Melón) <santiago.melon@kairosds.com>
2015-09-09 11:53:01 +0200

    Abstracted candidates config data to external config file + renamed & unified status codes

---
+++ app/conf/conf.js
@@ -0,0 +1,12 @@
+var statusCodes = [
+  {'cType': 'sAvai', 'cValue': 'Not contacted'},
+  {'cType': 'sWait', 'cValue': 'Waiting for reply'},
+  {'cType': 'sReq', 'cValue': 'Wants more info'},
+  {'cType': 'sWait', 'cValue': 'Meeting requested'},
+  {'cType': 'sWait', 'cValue': 'Meeting scheduled'},
+  {'cType': 'sReq', 'cValue': 'Evaluating'},
+  {'cType': 'sDone', 'cValue': 'Accepted'},
+  {'cType': 'sDone', 'cValue': 'Signed'},
+  {'cType': 'sNext', 'cValue': 'Wait some time'},
+  {'cType': 'sOff', 'cValue': 'Discarded'}
+];
